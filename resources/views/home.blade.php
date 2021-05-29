7ee00eac7e7c9c58f24a065d432ebf697c33a62e
ndaru21 <muhammad.ndaru@gmail.com>
2021-05-30 01:24:56 +0700

    last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,27 +5,33 @@
     @include('layouts.jumbotron')
     
     <div class="container mt-3">
-        <div class="row">
-            @foreach($barang as $data)
-            <div class="col-xl-3 col-6">
-                <div class="col-md-12 shadow-sm">
-                    <img src="{{ asset('storage/images/barang/'.$data->foto_barang) }}" alt="{{ $data->nama_barang }}" class="w-100">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $data->nama_barang }}</h5>
-                        <p class="card-text">Rp. {{ number_format($data->harga_barang) }}</p>
-                        <p class="card-text">
-                            Stok: 
-                            @if($data->jumlah_barang === '0' || $data->jumlah_barang == null)
-                                <span class="text-danger">Habis</span>
-                            @else
-                                {{ $data->jumlah_barang }}
-                            @endif
-                        </p>
-                        <a href="{{ route('showBarang', $data->id) }}" class="btn btn-primary text-white w-100">Lihat Selengkapnya <i class="uil uil-eye ms-1"></i></a>
+        @if($barang->count() == null)
+                <div class="alert alert-warning" role="alert">
+                    Barang masih kosong
+                </div>
+        @else
+            <div class="row">
+                @foreach($barang as $data)
+                <div class="col-xl-3 col-6 mb-3">
+                    <div class="col-md-12 shadow-sm">
+                        <img src="{{ asset('storage/images/barang/'.$data->foto_barang) }}" alt="{{ $data->nama_barang }}" class="w-100">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $data->nama_barang }}</h5>
+                            <p class="card-text">Rp. {{ number_format($data->harga_barang) }}</p>
+                            <p class="card-text">
+                                Stok: 
+                                @if($data->jumlah_barang === '0' || $data->jumlah_barang == null)
+                                    <span class="text-danger">Habis</span>
+                                @else
+                                    {{ $data->jumlah_barang }}
+                                @endif
+                            </p>
+                            <a href="{{ route('showBarang', $data->id) }}" class="btn btn-primary text-white w-100">Lihat <i class="uil uil-eye ms-1"></i></a>
+                        </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
+        @endif
     </div>
 @endsection
